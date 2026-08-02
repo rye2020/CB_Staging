d96208099949624b30c2de5577b72f112f6902bf
rye2020 <jrmarlatt@gmail.com>
2026-08-02 01:35:23 +0000

    clean functions-- stiop inserting styles

--- header.php
+++ header.php
@@ -57,8 +57,8 @@ $pagename = $wp_query->queried_object->post_name;
 		return;
 	}
 	else {  
-	echo '<link rel="stylesheet" id="child-style-css" href="https://www.us-covered-bonds.com/staging/3708/wp-content/themes/twentytwentyone-child/style.css?ver=1.0.0" media="all"> ';
-	echo '<link rel="stylesheet" id="child-style-css" href="https://www.us-covered-bonds.com/staging/3708/wp-content/themes/twentytwentyone-child/css/allpages.css?ver=1.0.0" media="all"> ';
+	echo '<link rel="stylesheet" id="child-style-css" href="https://www.us-covered-bonds.com/staging/3708/wp-content/themes/twentytwentyone-child/style.css" media="all"> ';
+	echo '<link rel="stylesheet" id="child-style-css" href="https://www.us-covered-bonds.com/staging/3708/wp-content/themes/twentytwentyone-child/css/allpages.css" media="all"> ';
 	 } 
 ?>
 <!--JRM - include sort table code -->
