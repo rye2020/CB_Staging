5a73b6e6c37aaa874756dcd14b82528af610b9be
rye2020 <jrmarlatt@gmail.com>
2026-08-04 23:20:23 +0000

    update  mat_year

--- includes/mat_year.php
+++ includes/mat_year.php
@@ -1,8 +1,8 @@
-<?php include("mat_year_mtn.php"); ?>
+<?php include( get_stylesheet_directory() . '/includes/mat_year_mtn.php'); ?>
 
 <?php 
 if (strpos($file, 'CBAgg_USD') !== 0) {
-echo "<label for='currencyselect'>Currency</label>";
+echo "<div class='filter_select'><label for='currencyselect'>Currency</label>";
 echo '<select name="currencyselect" style="width:60%">';
 echo "<option value=''> </option>";
 echo "<option value='$'>$</option>";
@@ -13,20 +13,19 @@ echo "<option value='A$'>A$</option>";
 echo "<option value='CHF'>CHF</option>";
 echo "<option value='NOK'>NOK</option>";
 echo "<option value='NOT $'>NOT $</option>";
-echo "</select>";
-echo "<br /> ";
+echo "</select></div>";
  } 
  ?>
 
-<label for='legacy'>Legislative</label>
+<div class="filter_select"><label for='legacy'>Legislative</label>
 <select name='legacy' style="width:60%;">
 <option value=''> </option>
 <option value='Legislative'>Legislative</option>
 <option value='Pre-Legislative'>Structured</option>
-</select>
-<br />
+</select></div>
 
-<label for='tenorselect'>Tenor</label>
+
+<div class="filter_select"><label for='tenorselect'>Tenor</label>
 <select name='tenorselect' style="width:60%">
 <option value=''> </option>
 <option value='1yr'>1yr</option>
@@ -36,15 +35,14 @@ echo "<br /> ";
 <option value='5yr'>5yr</option>
 <option value='7yr'>7yr</option>
 <option value='10yr'>10yr</option>
-</select>
-<br />
+</select></div>
 
-<label for='typeselect'>Type</label>
+
+<div class="filter_select"><label for='typeselect'>Type</label>
 <select name='typeselect' style="width:60%">
 <option value=''> </option>
 <option value='SEC'>SEC</option>
 <option value='144A'>144A</option>
 <option value='Reg S'>Reg S</option>
 <option value='BoC'>Bank of Canada</option>					 
-</select>
-<br />
+</select></div>
