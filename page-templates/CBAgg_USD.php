3ca2fc2ab9ce6b976af2ad5f636ef10d7a58d093
rye2020 <jrmarlatt@gmail.com>
2026-08-04 23:29:47 +0000

    update  mat_year

--- page-templates/CBAgg_USD.php
+++ page-templates/CBAgg_USD.php
@@ -87,12 +87,13 @@ $file = basename(__FILE__) . PHP_EOL;
 
       </div>
 
-      <div style="width:250px; ">
+      <div id="jmWrap">
+      <div id="jmfilter" style="width:250px; ">
          <br><br><br><br>
-         <?php include(get_stylesheet_directory() . '/includes/FilterUSDform.php'); ?>
+         <?php include(get_stylesheet_directory() . '/includes/FilterAGGform.php'); ?>
          <?php include(get_stylesheet_directory() . '/includes/RecentPosts_inc.php'); ?>
       </div>
-   </div>
+      </div>  <!-- End jmWrap-->
 </div>
 <?php echo do_shortcode("[jpshare]"); ?>
 <?php
