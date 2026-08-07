785a472c03268f461116600a47fa22258b22c7ab
rye2020 <jrmarlatt@gmail.com>
2026-08-07 21:38:47 +0000

    fix comments on functions

--- functions.php
+++ functions.php
@@ -482,7 +482,7 @@ function handle_agg_interest_form()
 }
 
 // for Claude.ai debugging Jan 2026 for block editor
-function custom_gallery_css()
+//function custom_gallery_css()
 // {
 //     echo '<style type="text/css">
 //     .wp-block-gallery.columns-4.is-layout-flex figure.wp-block-image {
