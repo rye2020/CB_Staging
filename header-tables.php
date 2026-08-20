86c8a6a8ca20b42a41b23659f134af24aa7a00bd
rye2020 <jrmarlatt@gmail.com>
2026-08-20 15:01:56 -0400

    Fix script source path for jm-mobile-tables

--- header-tables.php
+++ header-tables.php
@@ -61,7 +61,7 @@
 
 	<!--JRM - include sort table code -->
 	<script src="/wp-includes/js/sorttable.js" type="text/javascript"></script>
-	<script src="/js/jm-mobile-tables" type="text/javascript"></script>
+	<script src="js/jm-mobile-tables" type="text/javascript"></script>
 	<meta http-equiv="X-Frame-Options" content="sameorigin">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"> <!--JM 3/20-->
 
