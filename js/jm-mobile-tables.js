f487ff002b1c11a195b287125250e95b01b87a9a
rye2020 <jrmarlatt@gmail.com>
2026-08-21 01:16:56 +0000

    listen to window load

--- js/jm-mobile-tables.js
+++ js/jm-mobile-tables.js
@@ -1,7 +1,6 @@
     <!---Responsive tables---------------------------->
 	// Detect mobile device and change to single column
-    window.addEventListener('load', jmMobile);
-		
+    		
     screen.orientation.addEventListener('change', jmInsert); 
 		
 	function jmInsert() {
@@ -15,11 +14,9 @@
 			if (orientationType.includes ("portrait")) {
 				myDiv.remove();
 				myTopDiv.style.paddingLeft = "10px";
-				alert("Portrait");
 			} else {
 			if (!myDiv){
 				myTopDiv.style.paddingLeft = "40px";
-				alert("Landscape");
 				myWrap.innerHTML = `
 <div id="jm-filter" style="width:250px; ">
 	<br><br><br><br>
@@ -31,30 +28,42 @@
 			}
 		}	
 	};
-	}
+}
 
-  function jmMobile() {
-    var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    var myDiv = document.getElementById('jm-filter');
-	var myTopDiv = document.getElementById('top');
-    var tbHead = document.querySelector('.t1CanadianCBD');
-    const jmWidth = window.innerWidth;
-    
-	alert("width: " + jmWidth + "");
-	alert("isMobile: " + isMobile + "");
-	alert("Size: " + (jmWidth < 768) + "");
-	alert(myDiv);
-	alert(myTopDiv);
-	alert(tbHead);
-    if (isMobile && ( jmWidth < 768 ) ) {
-        myDiv.remove();         // Remove filters and recent posts
-		myTopDiv.style.paddingLeft = "10px";
-		tbHead.style.width = "100%";
-		alert(jmWidth);
-    } 
-	if (isMobile) { 
-		tbHead.style.width = "100%";
-		alert(jmWidth);
-	}
+$(document).ready(function () {
+	// Initialize DataTable
+	var table = $('#tbl-table').DataTable({
+		// Your DataTable options here
+		paging: true,
+		searching: true
+	});
+
+	// Fires once when DataTable is fully initialized
+	table.on('init.dt', function () {
+		alert("✅ DataTable initialization complete.");
+		// Place your code here that must run after table is ready
+		var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+		var myDiv = document.getElementById('jm-filter');
+		var myTopDiv = document.getElementById('top');
+		var tbHead = document.querySelector('.t1CanadianCBD');
+		const jmWidth = window.innerWidth;
+
+		alert("width: " + jmWidth + "");
+		alert("isMobile: " + isMobile + "");
+		alert("Size: " + (jmWidth < 768) + "");
+		alert(myDiv);
+		alert(myTopDiv);
+		alert(tbHead);
+		if (isMobile && (jmWidth < 768)) {
+			myDiv.remove();         // Remove filters and recent posts
+			myTopDiv.style.paddingLeft = "10px";
+			tbHead.style.width = "100%";
+			alert(jmWidth);
+		}
+		if (isMobile) {
+			tbHead.style.width = "100%";
+			alert(jmWidth);
+		}
+	})
 }
 	
