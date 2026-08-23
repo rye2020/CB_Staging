c388688e07dad4c4b8f3794f27eef36aa01d3895
rye2020 <jrmarlatt@gmail.com>
2026-08-23 21:06:28 +0000

    landscape problem

--- includes/mat_year_mtn.php
+++ includes/mat_year_mtn.php
@@ -1,9 +1,9 @@
 <div class="filter_select"><label for='issueyrselect'>Issue Year</label>
-<input type="text" name="issueyrselect"  style="width: 142px; height: 10px; border: 1px solid grey; " placeholder="">
+<input type="text" name="issueyrselect"  style="width: 57%; height: 10px; border: 1px solid grey; " placeholder="">
 <br /></div>
 
 <div class="filter_select"><label for='maturityyrselect'>Maturity Year</label>
-<input type="text" name="maturityyrselect"  style="width: 142px; height: 10px; border: 1px solid grey;" placeholder="    ">
+<input type="text" name="maturityyrselect"  style="width: 57%; height: 10px; border: 1px solid grey;" placeholder="">
 </div>
 
 <div class="filter_select"><label for='matured'>Matured</label>
