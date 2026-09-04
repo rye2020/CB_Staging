805bc0e84fce00b1ee66e3274cbef8313b8ef233
rye2020 <jrmarlatt@gmail.com>
2026-09-04 13:32:43 -0400

    Clean up header-CBDocs.php by removing comments
    Removed commented-out code for loading menu C.

--- header-CBDocs.php
+++ header-CBDocs.php
@@ -154,12 +154,7 @@
 				<?php /* Our navigation menu. If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assigned to the primary location is the one used. If one isn't assigned, the menu with the lowest ID is used. */ ?>
 				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 				<hr style="clear: both; background-color:white; height: 2px; margin: 0;" />
-					<?php 
-		//   JRM  load the tables menus   /////////////////////////////////////////////////
-                                       wp_nav_menu( array( 'theme_location' => 'primary', 
-                                                              'menu' => 'menu C') );
-   				
-                                 ?> 
+
 			</nav><!-- #access -->
 
 	</header><!-- #branding -->
