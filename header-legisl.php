3590150d77a0d0d3326b8fd604178bcc6505150b
rye2020 <jrmarlatt@gmail.com>
2026-09-04 13:47:14 -0400

    Clean up header-legisl.php by removing comments
    Removed commented-out code for loading legislation menus.

--- header-legisl.php
+++ header-legisl.php
@@ -157,12 +157,7 @@
 				<?php /* Our navigation menu. If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assigned to the primary location is the one used. If one isn't assigned, the menu with the lowest ID is used. */ ?>
 				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 				<hr style="clear: both; background-color:white; height: 2px; margin: 0;" />
-					<?php 
-		//   JRM  load the legislation menus   /////////////////////////////////////////////////
-                                       wp_nav_menu( array( 'theme_location' => 'primary', 
-                                                              'menu' => 'menu B') );
-   				
-                                 ?> 
+
 			</nav><!-- #access -->
 
 	</header><!-- #branding -->
